962c8cc197f9bbfb6d75c4cd670493025ac86856
Srikanth Kumar Karada <adamsrikanth0501@gmail.com>
2023-08-12 00:30:06 +0530

    swiggy cloned code copied

--- src/App.js
+++ src/App.js
@@ -3,22 +3,32 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main'>
+    <div className='left'>
+      <nav>
+        <img src='https://logos-world.net/wp-content/uploads/2020/11/Swiggy-Logo.png'></img>
+        <div className='btn'>
+          <button>Login</button>
+          <button className='signup'>Signup</button>
+        </div>
+      </nav>
+      <div className='txt'>
+      <h1>Game Night</h1>
+      <p>Order food from your favourite resturants</p>
+      <div className='inp'>
+        <input type='text' placeholder='Enter your delivery location'></input>
+        <button className='txtstyle'>FIND FOOD</button>
+      
+      </div>
+      
+      <p>Popular cities in India </p>
+      <p>Mumbai Chennai Bhubaneshwar Gujurat Andhapradesh Odisha</p>
     </div>
+        
+      </div>
+      
+    <img src='https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,h_1340/Lunch1_vlksgq'></img> 
+  </div>
   );
 }
 
